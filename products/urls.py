9609344dfe6afef7ba711ccb6167cffceb709326
dm1tro69 <dimolg22@gmail.com>
2020-04-05 01:42:27 +0300

    add first api

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name='product-list'),
-    path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
+    path('products/', views.product_list, name='product-list'),
+    path('products/<int:pk>/', views.product_detail, name='product-detail'),
+    path('manufactorers/<int:pk>/', views.manufacturer_detail, name='manufactor-detail'),
+    path('manufactorers/', views.manufacturer_list, name='manufactor-list'),
+
 ]
--- products/views.py
+++ products/views.py
@@ -1,16 +1,83 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
-from .models import Manufacturer, Product
 
-# Create your views here.
+from django.http import JsonResponse
 
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = 'products/product_detail.html'
+from .models import Product, Manufacturer
+
+def product_list(request):
+    products = Product.objects.all()#[:30]
+    data = {'products': list(products.values())}#'pk', 'name'
+    response = JsonResponse(data)
+    return response
+
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        data = {'product': {
+            'name': product.name,
+            'Manufacturer': product.manufacturer.name,
+            'Quantity': product.quantity,
+            'Description': product.description,
+            'Photo': product.photo.url,
+            'Price': product.price,
+            'Shipping cost': product.shipping_cost
+        }}
+        response = JsonResponse(data)
+    except Product.DoesNotExist:
+        response = JsonResponse({
+            'error': {
+                'code': 404,
+                'message': 'Product not found'
+            }
+        },
+        status=404)
+
+    return response
 
 
-class ProductListView(ListView):
-    model = Product
-    template_name = 'products/product_list.html'
+def manufacturer_list(request):
+    manufacturers = Manufacturer.objects.filter(active=True)
+    data = {'manufacturers': list(manufacturers.values())}#'pk', 'name'
+    response = JsonResponse(data)
+    return response
+
+
+def manufacturer_detail(request, pk):
+    try:
+        manufacturers = Manufacturer.objects.get(pk=pk)
+        manufacturer_product = manufacturers.products.all()
+        data = {'manufacturers': {
+            'name': manufacturers.name,
+            'location': manufacturers.location,
+            'active': manufacturers.active,
+            'products': list(manufacturers.products.values())
+
+        }}
+        response = JsonResponse(data)
+    except Manufacturer.DoesNotExist:
+        response = JsonResponse({
+            'error': {
+                'code': 404,
+                'message': 'manufacturers not found'
+            }
+        },
+        status=404)
+
+    return response
+
+
+
+# from django.views.generic.detail import DetailView
+# from django.views.generic.list import ListView
+#
+#
+#
+# class ProductDetailView(DetailView):
+#     model = Product
+#     template_name = 'products/product_detail.html'
+#
+#
+# class ProductListView(ListView):
+#     model = Product
+#     template_name = 'products/product_list.html'
 
